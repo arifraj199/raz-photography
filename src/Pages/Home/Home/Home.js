e8d807bd71a26dbec0fd83e9bc9c506281023dcb
arifraj199 <karif9514@gmail.com>
2022-04-17 00:04:51 +0600

    home page banner and route added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home Page</h2>
+            <div className="img-container text-white">
+                {/* <img width={'100%'} height={'600px'} src="https://i.ibb.co/WzngJdb/wedding-couple-france.png" alt="" /> */}
+                <h2> Take Best Weeding Photography</h2>
+                <p>Provide Best Service</p>
+                <button className='btn btn-primary'>Know More</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -13,9 +14,10 @@ const Header = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link href="#features">Services</Nav.Link>
-                <Nav.Link href="#pricing">Login</Nav.Link>
-                <Nav.Link href="#pricing">About</Nav.Link>
+                <Nav.Link as={Link} to="/service">Services</Nav.Link>
+                <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
