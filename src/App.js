7d91511f440a5ecab774210ccdc90e4692e540c4
arifraj199 <karif9514@gmail.com>
2022-04-17 19:11:49 +0600

    404 page implement

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Checkout from './Pages/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Signup from './Pages/Login/Signup/Signup';
+import NotFound from './Pages/NotFound/NotFound';
 import RequireAuth from './Pages/RequireAuth/RequireAuth';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -26,6 +27,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className='not-found'> 
+            <img src="https://i.ibb.co/rfc29jp/2392490.jpg" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
