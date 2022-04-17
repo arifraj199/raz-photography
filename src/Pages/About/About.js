90e43126f0d6bd7c078551e665ee3b6de67a0212
arifraj199 <karif9514@gmail.com>
2022-04-18 00:12:26 +0600

    about page implement

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
 
 const About = () => {
-    return (
-        <div>
-            <h2>This is About Page</h2>
+  return (
+    <div className="about-container">
+      <div className="about">
+          <div className="about-img">
+          <img src="https://i.ibb.co/CVPcQ3G/IMG-20190429-WA0064.jpg" alt="" />
+          </div>
+        
+        <div className="about-title">
+          <h3>Arif Hossain</h3>
+          <p>
+            Hi,Im Arif.I am now undergraduate engineering student.
+            Every people has a dream and i have also a dream.My dream is to become a web developer.I have dream't it when i was at college.I have learned tiny things related to web developement from online and now my goal and only goal is to reach of my destination and achieve success whatever it takes. 
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
